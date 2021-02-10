33117536e90af950a64ae44a0b63b4b35460233a
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-10 06:57:33 -0700

    rendering form without errors

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ContactForm from './ContactForm';
+
+test('render ContactForm without errors', () => {
+    render(<ContactForm />)
+});
